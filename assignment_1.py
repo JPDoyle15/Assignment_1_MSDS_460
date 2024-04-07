dd2ca535af74b0686c8f20882e0d993bd117f5cd
JPDoyle15 <166329063+JPDoyle15@users.noreply.github.com>
2024-04-07 14:54:29 -0500

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -1,19 +1,18 @@
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum, value
 
-# Initialize Class
 model = LpProblem("Minimize_Z", LpMinimize)
 
-# Define Decision Variables
+# Decision Variables
 a = LpVariable('a', lowBound=7, cat='Continuous')
 b = LpVariable('b', lowBound=7, cat='Continuous')
 c = LpVariable('c', lowBound=7, cat='Continuous')
 d = LpVariable('d', lowBound=7, cat='Continuous')
 e = LpVariable('e', lowBound=7, cat='Continuous')
 
-# Define Objective Function
+# Objective Function
 model += 0.74 * a + 2.83 * b + 0.09 * c + 0.79 * d + 1.00 * e
 
-# Define Constraints
+# Constraints
 model += 75*a + 65*b + 10*c + 135*d + 65*e <= 35000
 model += -110*a - 240*b - 160*c - 130*d - 25*e <= -14000
 model += -24*a - 23*b - 3*c - 8*d - 2*e <= -350
@@ -22,10 +21,9 @@ model += -10*a - 10*b - 320*d - 80*e <= -9100
 model += -0.4*a - 0.4*b - 2*c - 2.3*e <= -126
 model += -410*a - 410*b - 420*d - 470*e <= -32900
 
-# Solve Model
+# Model
 model.solve()
 
-# Print the results
 print(f"Chicken Breat = {round(a.varValue,2)} servings")
 print(f"Salmon = {round(b.varValue,2)} servings")
 print(f"White Rice = {round(c.varValue,2)} servings")
